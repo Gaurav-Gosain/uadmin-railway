4376a481099c05792e6a1654c0e24e63b1d34d0e
Gaurav Gosain <gg68@hw.ac.uk>
2022-09-11 17:26:25 +0400

    testing out environment variables on railway

--- main.go
+++ main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
-    "github.com/uadmin/uadmin"
+	"os"
+	"strconv"
+
+	"github.com/uadmin/uadmin"
 )
 
 func main() {
-    uadmin.StartServer()
+
+	// Map environment variables to the database config
+
+	dbtype := "mysql"
+	host := os.Getenv("MYSQLHOST")
+	port, err := strconv.Atoi(os.Getenv("MYSQLPORT"))
+	if err != nil {
+		// ... handle error
+		panic(err)
+	}
+	user := os.Getenv("MYSQLUSER")
+	password := os.Getenv("MYSQLPASSWORD")
+	name := os.Getenv("MYSQLDATABASE")
+
+	// Connect to the database
+	uadmin.Database = &uadmin.DBSettings{
+		Type:     dbtype,
+		Host:     host,
+		Port:     port,
+		User:     user,
+		Password: password,
+		Name:     name,
+	}
+
+	uadmin.StartServer()
 }
