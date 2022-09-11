e40bd46ceeedad0bbefcfa0b827389ee631b192a
Gaurav Gosain <gg68@hw.ac.uk>
2022-09-11 17:43:20 +0400

    added local database config support

--- main.go
+++ main.go
@@ -1,16 +1,42 @@
 package main
 
 import (
+	"encoding/json"
+	"io"
 	"os"
 	"strconv"
 
 	"github.com/uadmin/uadmin"
 )
 
-func main() {
+func InitDatabase() *uadmin.DBSettings {
+
+	// Check if a local .database file exists
+	if _, err := os.Stat(".database"); err == nil {
+		// If it does, read the file and return the database settings
+
+		// Step 1: Open the file
+		databaseConfigFile, err := os.Open(".database")
+		if err != nil {
+			panic(err)
+		}
+
+		byteValue, _ := io.ReadAll(databaseConfigFile)
+
+		// Step 2: Unmarshal the file
+		var DBSettings uadmin.DBSettings
+
+		err = json.Unmarshal(byteValue, &DBSettings)
+
+		if err != nil {
+			panic(err)
+		}
+		defer databaseConfigFile.Close()
+
+		return &DBSettings
+	}
 
 	// Map environment variables to the database config
-
 	dbtype := "mysql"
 	host := os.Getenv("MYSQLHOST")
 	port, err := strconv.Atoi(os.Getenv("MYSQLPORT"))
@@ -23,7 +49,7 @@ func main() {
 	name := os.Getenv("MYSQLDATABASE")
 
 	// Connect to the database
-	uadmin.Database = &uadmin.DBSettings{
+	return &uadmin.DBSettings{
 		Type:     dbtype,
 		Host:     host,
 		Port:     port,
@@ -31,6 +57,11 @@ func main() {
 		Password: password,
 		Name:     name,
 	}
+}
+
+func main() {
+
+	uadmin.Database = InitDatabase()
 
 	uadmin.StartServer()
 }
